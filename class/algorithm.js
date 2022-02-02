96d27783bf7f1ab4864fc62e9f4b293c061d70a3
Isaac <isaac.k.sage@gmail.com>
2022-02-01 21:06:31 -0800

    Flex Navbar assignment complete

--- class/algorithm.js
+++ class/algorithm.js
@@ -1,51 +1,91 @@
-function d6() {
-    var roll = Math.random();
-    roll = 6 * roll;
-    roll = Math.floor(roll) + 1;
-    return roll;
-}
 
-var playerRoll = d6();
-console.log("The player rolled: " + playerRoll);
+// Our arrays of objects
 
 
-
-
-
-
-
-
-
-// Consult the Oracle
-
-
-var lifesAnswers = [
-    "It is certain.",
-    "It is decidedly so.",
-    "Without a doubt.",
-    "Yes – definitely.",
-    "You may rely on it.",
-    "As I see it, yes.",
-    "Most likely.",
-    "Outlook good.",
-    "Yes.",
-    "Signs point to yes.",
-    "Reply hazy, try again.",
-    "Ask again later.",
-    "Better not tell you now.",
-    "Cannot predict now.",
-    "Concentrate and ask again.",
-    "Don't count on it.",
-    "My reply is no.",
-    "My sources say no.",
-    "Outlook not so good.",
-    "Very doubtful."
+var pokémon = [
+    { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+    { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+    { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+    { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+    { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+    { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+    { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+    { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+    { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+    { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+    { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+    { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+    { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+    { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+    { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+    { "id": 98,  "name": "Krabby",     "types": ["water"] },
+    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+    { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+    { "id": 133, "name": "Eevee",      "types": ["normal"] },
+    { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+    { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+    { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+    { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
 ];
 
-function fortuneTeller(answerArr) {
-    var randomSelection = Math.floor(answerArr.length * Math.random());
-    console.log(answerArr[randomSelection]);
-    return answerArr[randomSelection];
+
+// console.log() Pokemons with index greater than 99
+
+// for(var i=0; i<pokémon.length; i++) {
+//     if(pokémon[i].id > 99) {
+//         console.log(pokémon[i].name);
+//     }
+// }
+
+// Challenges - using the array of pokémon above and loops:
+
+// 1. console.log the pokémon objects whose id is evenly divisible by 3
+
+for (var i = 0; i < pokémon.length; i++) {
+    if (pokémon[i].id % 3 === 0) {
+        console.log("ID is evenly divisible by 3:", pokémon[i])
+    }
 }
 
-fortuneTeller(lifesAnswers);
+// 2. console.log the pokémon objects that have more than one type
+
+for (var i = 0; i < pokémon.length; i++) {
+    if (pokémon[i].types.length > 1) {
+        console.log("pokemons with more than one type:", pokémon[i])
+    }
+}
+
+// 3. console.log the names of the pokémon whose only type is "poison"
+
+for (var i = 0; i < pokémon.length; i++) {
+    // console.log(pokémon[i].types.length)
+    // console.log(pokémon[i].types)
+    if (pokémon[i].types.length == 1 && pokémon[i].types[0] == "poison") {
+        console.log("poison Pokemon:", pokémon[i].name)
+    }
+}
+
+// 4. console.log the first type of all the pokémon whose second type is "flying"
+
+for (var i = 0; i < pokémon.length; i++) {
+    if (pokémon[i].types.length == 2 && pokémon[i].types[1] == "flying") {
+        console.log("Mystic bird:", pokémon[i].name, "-type:", pokémon[i].types[0])
+    }
+}
+
+// Bonus Challenge: console.log the reverse of the names of the pokémon whose only type is "poison"
+
+for (var i = 0; i < pokémon.length; i++) {
+    // console.log(pokémon[i].types.length)
+    // console.log(pokémon[i].types)
+    if (pokémon[i].types.length == 1 && pokémon[i].types[0] == "poison") {
+        var poisonIntoMedicine = "";
+        for (var j = pokémon[i].name.length - 1; j >= 0; j--){
+            poisonIntoMedicine += (pokémon[i].name[j]);
+            // console.log("poison Pokemon:", pokémon[i].name)
+        }
+        poisonIntoMedicine = poisonIntoMedicine.toLowerCase();
+
+    console.log("reverse the poison:", poisonIntoMedicine)
+    }
+}
